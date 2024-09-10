17e98017a7cd35c1e31ceb91e382fd5547d58e8c
umoren <samuelumoren365@gmail.com>
2024-09-10 15:13:30 +0100

    Latest changes

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -115,7 +115,7 @@ const AlertDialogCancel = React.forwardRef<
   <AlertDialogPrimitive.Cancel
     ref={ref}
     className={cn(
-      buttonVariants({ variant: "outline" }),
+      buttonVariants({ variant: "muted" }),
       "mt-2 sm:mt-0",
       className
     )}
